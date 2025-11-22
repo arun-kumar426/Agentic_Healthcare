64c04de1832771490a44854d2c5368b4d37580f0
arun-kumar426 <arunkumarofficial426@gmail.com>
2025-11-22 16:29:17 +0530

    Update llm.py

--- src/llm.py
+++ src/llm.py
@@ -1,5 +1,6 @@
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
-from .config import GROQ_API_KEY, LLM_MODEL_NAME
+from .config import GROQ_API_KEY, EMBEDDING_MODEL_NAME, LLM_MODEL_NAME
 
 def get_llm():
     return ChatGroq(
@@ -7,3 +8,6 @@ def get_llm():
         model_name=LLM_MODEL_NAME,
         temperature=0.2
     )
+
+def get_embeddings():
+    return HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
